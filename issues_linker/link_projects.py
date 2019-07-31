fbcad3a8515f53ef34eb7dd734564dfebaf83587
AlexanderND <52403346+AlexanderND@users.noreply.github.com>
2019-07-31 11:36:44 +0500

    Добавил ведение логов при привязке проектов

--- issues_linker/link_projects.py
+++ issues_linker/link_projects.py
@@ -8,6 +8,7 @@ from issues_linker.quickstart.models import Linked_Projects         # связа
 from issues_linker.quickstart.models import Linked_Issues           # связанные issues
 
 from issues_linker.my_functions import WRITE_LOG                    # ведение логов
+from issues_linker.my_functions import WRITE_LOG_ERR                # ведение логов ошибок
 from issues_linker.my_functions import align_special_symbols        # обработка спец. символов (\ -> \\)
 from issues_linker.my_functions import read_file                    # загрузка файла (возвращает строку)
 
@@ -131,73 +132,96 @@ def link_projects(payload):
     labels_url_gh = api_url_gh + '/labels'
 
     # label-ы, необходимые для работы связи
-    # (да, немного некрасиво выглядит)
+    # (да, немного костыль)
     labels = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+    #labels = [{}]  # для тестов
 
     labels[0]['name'] = 'Priority: low'
     labels[0]['description'] = 'Low priority issue'
     labels[0]['color'] = 'ffe99c'
+    labels[0]['default'] = 'false'
 
     labels[1]['name'] = 'Priority: normal'
     labels[1]['description'] = "Most issues should have 'normal' priority"
     labels[1]['color'] = 'ffdb5e'
+    labels[1]['default'] = 'true'
 
     labels[2]['name'] = 'Priority: urgent'
     labels[2]['description'] = 'Urgent issue'
     labels[2]['color'] = 'ffc600'
+    labels[2]['default'] = 'false'
 
     labels[3]['name'] = 'Status: feedback'
     labels[3]['description'] = 'We are awaiting your feedback on the issue'
     labels[3]['color'] = '85ffb0'
+    labels[3]['default'] = 'false'
 
     labels[4]['name'] = 'Status: new'
     labels[4]['description'] = 'Default status for new issues'
     labels[4]['color'] = '2b57ff'
+    labels[4]['default'] = 'true'
 
     labels[5]['name'] = 'Status: rejected'
     labels[5]['description'] = 'Issue rejected'
     labels[5]['color'] = 'a80000'
+    labels[5]['default'] = 'false'
 
     labels[6]['name'] = 'Status: verification'
     labels[6]['description'] = 'We are verifying, that the issue has been resolved'
     labels[6]['color'] = 'c9ffdc'
+    labels[6]['default'] = 'false'
 
     labels[7]['name'] = 'Status: working'
     labels[7]['description'] = 'We are working on it. Please, be patient!'
     labels[7]['color'] = '38ff7e'
+    labels[7]['default'] = 'false'
 
     labels[8]['name'] = 'Tracker: bug'
     labels[8]['description'] = "Something isn't working"
     labels[8]['color'] = 'e00000'
+    labels[8]['default'] = 'false'
 
     labels[9]['name'] = 'Tracker: task'
     labels[9]['description'] = 'Suggestions or the like'
     labels[9]['color'] = '2b57ff'
+    labels[9]['default'] = 'true'
 
     response_text = 'Projects posted successfully!\n' +\
-                    "(or not, I actually don't know)\n"+\
+                    "(or not, I actually don't know)\n" +\
                     "Labels:\n\n"
 
-    # TODO: исправить постинг label-ов (некорректно приходит description)
+    # TODO: исправить постинг label-ов (не приходит description)
+    # TODO: исправить постинг label-ов (не приходит default)
     # постим label-ы в гитхаб
     for label in labels:
 
+        # загружаем label-ы
         label_templated = create_label_github_template.render(
             name=label['name'],
             description=label['description'],
-            color=label['color'])
+            color=label['color'],
+            default=label['default'])
 
         # кодировка Latin-1 на некоторых задачах приводит к ошибке кодировки в питоне
         label_templated = label_templated.encode('utf-8')
 
-        request_result = requests.post(labels_url_gh,
-                                       data=label_templated,
-                                       headers=headers_gh)
-        response_text += label['name'] + '\n'
-        response_text += str(request_result) + '\n'
-        response_text += str(request_result.text) + '\n'
+        post_result = requests.post(labels_url_gh,
+                                    data=label_templated,
+                                    headers=headers_gh)
+
+        log_text= ''
+        log_text += label['name'] + '\n'
+        log_text += str(post_result) + '\n'
+        log_text += str(post_result.text) + '\n'
+
+        if (post_result.status_code == 201):
+            WRITE_LOG(log_text)
+
+        else:
+            WRITE_LOG_ERR(log_text)
+
+        response_text += log_text
 
 
-    WRITE_LOG(response_text)
     # TODO: выдавать то же, что выдавалось бы без переопределения метода create
-    return HttpResponse(response_text, status=200)
+    return HttpResponse(response_text.replace('\n', '<br>'), status=200)    # <br> - новая строка в html
